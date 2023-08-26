565347a9be8c7846748a8ffd9fd4c778e2ada3a8
Masoud Mohajeri <m.mohajeri@bluteam.ir>
2023-08-26 10:29:39 +0330

    clean video codes

--- src/components/face-landmark/v2.index.tsx
+++ src/components/face-landmark/v2.index.tsx
@@ -3,29 +3,16 @@ import { type FC, useRef, useEffect, useState } from 'react'
 import styles from './index.module.scss'
 import { videoHeight, videoWidth } from './constants'
 // import useAudioClassifier from '../../hooks/useAudioClassifier'
-import useVideoLandmark from '../../hooks/useVideoLandmark'
-import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts'
 
 const FaceLandmark: FC = () => {
   const video = useRef<HTMLVideoElement | null>(null)
   const canvasRef = useRef<HTMLCanvasElement | null>(null)
   const [isVideoElementReady, setIsVideoElementReady] = useState(false)
   const [isStreamReady, setIsStreamReady] = useState(false)
-  const { isVideoAnalyzerReady, setVideoStreamFrameRate, startProcess, stopProcess, result } = useVideoLandmark({
-    onResult: drawResults,
-    videoElement: video.current,
-    canvasElement: canvasRef.current,
-  })
 
   useEffect(() => {
     startCamera()
   }, [isVideoElementReady])
-
-  useEffect(() => {
-    if (isVideoAnalyzerReady && isStreamReady) {
-      startProcess()
-    }
-  }, [isVideoAnalyzerReady, isStreamReady])
 
   // can all this function become a giant promise ?
   const startCamera = async () => {
@@ -44,7 +31,7 @@ const FaceLandmark: FC = () => {
         () => {
           if (!video?.current) return
           video.current.play()
-          setVideoStreamFrameRate(currentStream?.getSettings()?.frameRate || 30)
+          // setVideoStreamFrameRate(currentStream?.getSettings()?.frameRate || 30)
           setIsStreamReady(true)
         },
         { once: true },
@@ -88,81 +75,11 @@ const FaceLandmark: FC = () => {
         height={videoHeight}
         style={{ border: '1px solid black' }}
       />
-      <button onClick={startProcess}>startProcess</button>
+      {/*
+       <button onClick={startProcess}>startProcess</button>
       <button onClick={stopProcess}>stopProcess</button>
+     */}
       {/* <pre>{JSON.stringify(extractedData, null, 2)}</pre> */}
-      <ResponsiveContainer width="100%" height="100%">
-        <LineChart
-          width={500}
-          height={300}
-          // @ts-ignore
-          data={result.map((item, index) => ({ ...item.modelOut, index }))}
-          margin={{
-            top: 5,
-            right: 30,
-            left: 20,
-            bottom: 5,
-          }}>
-          <CartesianGrid strokeDasharray="3 3" />
-          <XAxis dataKey="index" />
-          <YAxis />
-          <Tooltip />
-          <Legend />
-          <Line isAnimationActive={false} type="monotone" dataKey="mouthPucker" stroke="#8884d8" activeDot={{ r: 8 }} />
-          <Line isAnimationActive={false} type="monotone" dataKey="mouthShrugLower" stroke="#82ca9d" />
-          <Line isAnimationActive={false} type="monotone" dataKey="mouthFunnel" stroke="#BB2525" />
-        </LineChart>
-      </ResponsiveContainer>
-      <ResponsiveContainer width="100%" height="100%">
-        <LineChart
-          width={500}
-          height={300}
-          // @ts-ignore
-          data={result.map((item, index) => ({ ...item.modelOut, index }))}
-          margin={{
-            top: 5,
-            right: 30,
-            left: 20,
-            bottom: 5,
-          }}>
-          <CartesianGrid strokeDasharray="3 3" />
-          <XAxis dataKey="index" />
-          <YAxis />
-          <Tooltip />
-          <Legend />
-          <Line
-            isAnimationActive={false}
-            type="monotone"
-            dataKey="eyeBlinkLeft"
-            stroke="#8884d8"
-            activeDot={{ r: 8 }}
-          />
-          <Line isAnimationActive={false} type="monotone" dataKey="eyeBlinkRight" stroke="#82ca9d" />
-        </LineChart>
-      </ResponsiveContainer>
-      <ResponsiveContainer width="100%" height="100%">
-        <LineChart
-          width={500}
-          height={300}
-          // @ts-ignore
-          data={result.map((item, index) => ({ ...item.areaRatiosPercentage, index }))}
-          margin={{
-            top: 5,
-            right: 30,
-            left: 20,
-            bottom: 5,
-          }}>
-          <CartesianGrid strokeDasharray="3 3" />
-          <XAxis dataKey="index" />
-          <YAxis />
-          <Tooltip />
-          <Legend />
-
-          <Line isAnimationActive={false} type="monotone" dataKey="leftEyeToFace" stroke="#8884d8" />
-          <Line isAnimationActive={false} type="monotone" dataKey="rightEyeToFace" stroke="#82ca9d" />
-          {/* <Line isAnimationActive={false} type="monotone" dataKey="lipsToFace" stroke="#FAF1E4" /> */}
-        </LineChart>
-      </ResponsiveContainer>
     </div>
   )
 }
